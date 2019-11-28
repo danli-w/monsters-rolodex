09c35353ecbb7105277f6c3e313f1f6783922be4
danli-w <wdlforeverlove@gmail.com>
2019-11-27 22:33:49 -0600

    Updates

---
+++ precache-manifest.e87b05f9dc5b87e7286019a1c98d3d63.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1c984c95994ec0929085c6e7515246e",
+    "url": "/danli-w/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3b70666772e996fda78b",
+    "url": "/danli-w/monsters-rolodex/static/css/main.fc70b046.chunk.css"
+  },
+  {
+    "revision": "072144937a437c299fb0",
+    "url": "/danli-w/monsters-rolodex/static/js/2.12dc0d94.chunk.js"
+  },
+  {
+    "revision": "3b70666772e996fda78b",
+    "url": "/danli-w/monsters-rolodex/static/js/main.e03b27b2.chunk.js"
+  },
+  {
+    "revision": "08fa9acccce9658490c5",
+    "url": "/danli-w/monsters-rolodex/static/js/runtime-main.a80b24dc.js"
+  }
+]);
